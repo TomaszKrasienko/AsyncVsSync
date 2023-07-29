44b270396987934bcdc503e24167760fe9b41997
Tomasz Krasieńko <tomasz.krasienko@outlook.com>
2023-07-29 22:32:58 +0200

    Added Performance tests project

---
+++ AsyncVsSync/SyncApi.PerformanceTests/SyncApiTests.cs
@@ -0,0 +1,40 @@
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+
+namespace SyncApi.PerformanceTests;
+
+public class SyncApiTests
+{
+    public SyncApiTests()
+    {
+        
+    }
+    
+    [Fact]
+    public void Get_PerformanceTests()
+    {
+        const string url = "http://localhost:5267";
+        var getSyncApiStep = Step.Create("get", HttpClientFactory.Create(), async context =>
+        {
+            try
+            {
+                var req = Http.CreateRequest("GET", url);
+                return await Http.Send(req, context);
+            }
+            catch
+            {
+                return Response.Fail();
+            }
+        });
+
+        var scenario = ScenarioBuilder.CreateScenario("Sync api fetch", getSyncApiStep)
+            .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+            .WithLoadSimulations(LoadSimulation.NewKeepConstant(100, TimeSpan.FromSeconds(5)));
+
+        var stats = NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+        
+    }
+}
--- AsyncVsSync/SyncApi.PerformanceTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace SyncApi.PerformanceTests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
