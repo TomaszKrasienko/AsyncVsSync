c454ad121dd260e82e6f786d2ad8c1ad2524e72a
Tomasz Krasieńko <tomasz.krasienko@outlook.com>
2023-07-30 08:29:21 +0200

    Added tests

---
+++ AsyncVsSync/AsyncAPI.PerformanceTests/AsyncApiTests.cs
@@ -0,0 +1,40 @@
+﻿using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+
+namespace AsyncAPI.PerformanceTests;
+
+public class AsyncApiTests
+{
+    public AsyncApiTests()
+    {
+        
+    }
+    
+    [Fact]
+    public void Get_PerformanceTests()
+    {
+        const string url = "http://localhost:5267";
+        var getSyncApiStep = Step.Create("get", HttpClientFactory.Create(), async context =>
+        {
+            try
+            {
+                var req = Http.CreateRequest("GET", url);
+                return await Http.Send(req, context);
+            }
+            catch
+            {
+                return Response.Fail();
+            }
+        });
+
+        var scenario = ScenarioBuilder.CreateScenario("Async api fetch", getSyncApiStep)
+            .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+            .WithLoadSimulations(LoadSimulation.NewKeepConstant(100, TimeSpan.FromSeconds(5)));
+
+        var stats = NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+        
+    }
+}
